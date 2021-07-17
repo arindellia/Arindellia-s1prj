d22cca04ef530d2526d4431656d5d7576b8fe779
Stepan Lyashev <dodvp@mail.ru>
2021-07-17 19:20:14 +0300

    init

---
+++ Arindellia's1prj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arindellia_s1prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
